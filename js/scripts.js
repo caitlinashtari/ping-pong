ca4d7abfd2ebb040223b55ff2ddcbf09e2b690fb
Caitlin Ashtari <caitlinashtari@Unknown.domain>
2016-10-07 22:53:17 -0700

    add conditionals to js that determine divisibility of numbers and return ping, pong, pingpong values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,32 @@
 /* Back-end ----------------*/
+var numberArray = [];
 
 
-
-
-
-
-
-
-
+var number = function (numberInput){
+  if (numberInput % 3 === 0 && numberInput % 15 != 0){
+    return "ping";
+  } else if (numberInput % 5 === 0 && numberInput % 15 != 0){
+    return "pong";
+  } else if (numberInput % 15 === 0){
+    return "pingpong";
+  } else {
+    return numberInput;
+  }
+};
 
 
 
 /* Front-end ----------------*/
 $(document).ready(function(){
-  $("#formDiv form").submit(function(event){
-    var numberInput = $("input#numberInput").val();
-
-    $(".output").show();
+  $(".formDiv form").submit(function(event) {
     event.preventDefault();
+    var numberInput = parseInt($("input#numberInput").val());
+    numberArray.push(numberInput);
+    var result = number(numberInput);
+
+
+    $(".output").text(result);
+
   });
 
 
